82de294dbdcf6a4e1d1cf6c8260e574768a9e03d
Phoenix <fphoenix923@hotmail.com>
2016-07-10 16:15:51 +0800

    建立PotterShoppingCart類別

---
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+	public class PotterShoppingCart
+	{
+		public void BuyFirstEpisode(int v)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object Payables()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
