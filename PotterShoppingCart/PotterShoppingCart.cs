920550525ba937d86804abb3ca7e0b450a60ab9c
Phoenix <fphoenix923@hotmail.com>
2016-07-10 16:57:55 +0800

    完成實作：一二三四集各買了一本，價格應為100*4*0.8=320

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -47,7 +47,9 @@ namespace PotterShoppingCart
 			var discount = 1M;
 			var books = this.m_Potters.Where(p => p.Value > 0).Count();
 
-			if (books >= 3)
+			if (books >= 4)
+				discount = 0.8M;
+			if (books == 3)
 				discount = 0.9M;
 			else if (books == 2)
 				discount = 0.95M;
