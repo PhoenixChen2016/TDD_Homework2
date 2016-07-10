2ffdc26315752d97017a3ee44c31d3f291134714
Phoenix <fphoenix923@hotmail.com>
2016-07-10 16:05:46 +0800

    建立測試類別

---
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+	[TestClass]
+	public class PotterShoppingCartTests
+	{
+		[TestMethod]
+		public void PotterShopping_只買第一集價格是100元()
+		{
+
+		}
+	}
+}
