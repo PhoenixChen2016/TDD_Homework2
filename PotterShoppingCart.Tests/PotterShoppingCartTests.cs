407a57d9c4c224a5faf0d53cee801e5868b836b1
Phoenix <fphoenix923@hotmail.com>
2016-07-10 17:01:12 +0800

    完成測試：一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375

--- PotterShoppingCart.Tests/PotterShoppingCartTests.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTests.cs
@@ -83,5 +83,26 @@ namespace PotterShoppingCart.Tests
 
 			Assert.AreEqual(expected, actual);
 		}
+
+		[TestMethod]
+		public void PotterShopping_一次買了整套_一二三四五集各買了一本_價格應該打75折為375元()
+		{
+			// arrange
+			var sut = new PotterShoppingCart();
+
+			sut.BuyFirstEpisode(1);
+			sut.BuySecondEpisode(1);
+			sut.BuyThirdEpisode(1);
+			sut.BuyForthEpisode(1);
+			sut.BuyFifthEpisode(1);
+
+			// act
+			var actual = sut.Payables();
+
+			// assert
+			var expected = 375;
+
+			Assert.AreEqual(expected, actual);
+		}
 	}
 }
