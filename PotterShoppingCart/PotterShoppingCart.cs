9ca3bddfa4070615444932f57ece4281362db229
Phoenix <fphoenix923@hotmail.com>
2016-07-10 16:27:10 +0800

    刪除Potter，只是記錄本數不用再多出類別定義

--- PotterShoppingCart/PotterShoppingCart.cs
+++ PotterShoppingCart/PotterShoppingCart.cs
@@ -7,9 +7,18 @@ namespace PotterShoppingCart
 {
 	public class PotterShoppingCart
 	{
+		private Dictionary<string, int> m_Potters = new Dictionary<string, int>
+		{
+			["哈利波特1"] = 0,
+			["哈利波特2"] = 0,
+			["哈利波特3"] = 0,
+			["哈利波特4"] = 0,
+			["哈利波特5"] = 0,
+		};
+
 		public void BuyFirstEpisode(int quantity)
 		{
-			throw new NotImplementedException();
+			
 		}
 
 		public object Payables()
